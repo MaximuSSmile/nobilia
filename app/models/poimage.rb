0e84f84aa49318a2317cb13cf7f6eb9cf4665d0b
maximussmile <maximusgranddeveloper@gmail.com>
2017-07-03 09:45:42 +0300

    some style portfolio form pageee

--- app/models/poimage.rb
+++ app/models/poimage.rb
@@ -2,6 +2,6 @@ class Poimage < ActiveRecord::Base
   belongs_to :portfolio
 
     	has_attached_file :pimg, styles: { :medium => "600x400#", # :smale => "600x400>",
-    	 :hard => "2400x1400#" }
+    	 :hard => "2400x1200#" }
 	validates_attachment_content_type :pimg, content_type: /\Aimage\/.*\Z/
 end
