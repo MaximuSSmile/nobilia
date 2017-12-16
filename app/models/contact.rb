fca815530afb2af03729a3a3cc34e5d7c94d4952
maximussmile <maximusgranddeveloper@gmail.com>
2017-12-16 06:59:53 +0200

    add phone contact form2

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -2,7 +2,7 @@
 
 
 class Contact < MailForm::Base
-  attribute :name,      :validate => true
+  attribute :name, subject: => "Имя Клиента",     :validate => true
   attribute :email,     :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
 
   attribute :phonenumber,   :validate => true
