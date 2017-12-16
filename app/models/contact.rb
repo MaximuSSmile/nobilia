d9c7f13f875d43ff268970095cf80bca9c1178d2
maximussmile <maximusgranddeveloper@gmail.com>
2017-12-16 07:33:41 +0200

    add phone contact form name add1

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -15,9 +15,8 @@ class Contact < MailForm::Base
   def headers
     {
       :subject => "Желаю Кухню NOBILIA",
-      :name => "Имя Клиента",
       #:to => "nobilia.odessa@gmail.com",
-      :to => "klerikmaxxx@gmail.com",
+      :to => "klerikmaxxx@gmail.com", :name => "Имя Клиента",
       :from => %("#{name}" <#{email}>)
     }
   end
