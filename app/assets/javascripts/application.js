b6a7bc3360c290d7e70cd4dcf65ee54579141ed7
maximussmile <maximusgranddeveloper@gmail.com>
2017-12-29 17:17:46 +0200

    furn schilling rasdy

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -54,3 +54,66 @@ $(document).ready(function(){
 
   });
 });
+
+
+
+$(document).ready(function(){
+  $('.furnitur-schilling-carusel').slick({
+  width: 300,
+  arrows: false,
+  
+  autoplay: true,
+    autoplaySpeed: 0,
+    speed: 2500,
+    cssEase: 'linear',
+
+    pauseOnFocus: true,
+    pauseOnHover: true,
+    pauseOnDotsHover: true,
+  
+  dots: true,
+  infinite: true,
+
+  variableWidth: true,
+
+  });
+});
+
+
+//   ###############   COMMENTS  BEGIN  ############## */ 
+
+
+    $(document).ready(function(){
+
+      // Плавный скролл по якорям
+      $('a[href^="#"]').click(function () { 
+          elementClick = $(this).attr("href");
+        destination = $(elementClick).offset().top;
+        if($.browser.safari){
+        $('body').animate( { scrollTop: destination }, 1000 );
+        } else {
+            $('html').animate( { scrollTop: destination }, 1000 );
+        }
+        return false;
+       });
+    });
+
+
+
+
+$(document).ready(function(){
+    $("#furnitur_lozung1").on("click","a", function (event) {
+        //отменяем стандартную обработку нажатия по ссылке
+        event.preventDefault();
+ 
+        //забираем идентификатор бока с атрибута href
+        var id  = $(this).attr('href'),
+ 
+        //узнаем высоту от начала страницы до блока на который ссылается якорь
+            top = $(id).offset().top;
+         
+        //анимируем переход на расстояние - top за 1500 мс
+        $('body,html').animate({scrollTop: top}, 1500);
+    });
+});
+// ###############   COMMENTS END  ############## 
